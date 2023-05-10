4c277f395454594e0bf94aaf5fd3c44a8b598695
Muhammed Huseyn <salamovsalam1w@gmail.com>
2023-05-10 16:17:09 +0400

    mhuseyn.deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ExploreComponent } from './explore/explore.component';
-// import { ItemdetailsComponent } from './Itemdetails/Itemdetails.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  // { path: 'itemdetails', component: ItemdetailsComponent },
   { path: 'explore', component: ExploreComponent },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' },
-  // { path: '**', component: PageNotFoundComponent }
+  { path: "item-details", component: ItemDetailsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
 ];
 
 @NgModule({
